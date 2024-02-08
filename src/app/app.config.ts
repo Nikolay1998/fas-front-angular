057938b04d9a5ed8560bbb2496171ec90768bf3e
kraynov.n <kriverson13@gmail.com>
2024-02-08 15:46:40 +0400

    Logout on 401,404 http response, minor improvements.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { basicAuthInterceptor } from './_helpers/basic-auth.interceptor';
 import { routes } from './app.routes';
+import { errorInterceptor } from './_helpers/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([basicAuthInterceptor]),
+      withInterceptors([basicAuthInterceptor, errorInterceptor]),
     ),
   ]
 };
